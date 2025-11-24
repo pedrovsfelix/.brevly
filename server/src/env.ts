986104c7a63a2114a6fc139c25c83ca532c95660
pedrovsfelix <seuemail@example.com>
2025-11-24 19:19:53 -0300

    :sparkles: feat: Criando arquivo com as variáveis de ambiente.

---
+++ server/src/env.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+	PORT: z.coerce.number().default(3333),
+	NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+	DATABASE_URL: z
+		.string()
+		.url("Insira uma URL válida!")
+		.startsWith("postgresql://"),
+
+	CLOUDFLARE_ACCOUNT_ID: z.string(),
+	CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+	CLOUDFLARE_SECRET_ACESS_KEY: z.string(),
+	CLOUDFLARE_BUCKET: z.string(),
+	CLOUDFLARE_PUBLIC_URL: z.string().url("Insira uma URL válida!"),
+});
+
+export const env = envSchema.parse(process.env);
