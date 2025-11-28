90270c5f8b918fc27957d12f3aa88f48feb36268
pedrovsfelix <pedrovitorsf@gmail.com>
2025-11-28 06:35:59 -0300

    :sparkles: feat: Criando lógica para exportar o arquivo .csv.

--- server/src/infra/http/routes/export-link.ts
+++ server/src/infra/http/routes/export-link.ts
@@ -1,7 +1,36 @@
+import { exportUploads } from "@/app/functions/export-links";
+import { unwrapEither } from "@/shared/either";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import z from "zod";
 
-export const exportLinkRoute: FastifyPluginAsyncZod = async (server) => {
-	server.post("/links", () => {
-		return "hello";
-	});
+export const exportLinkRoute: FastifyPluginAsyncZod = async server => {
+    await server.post(
+        "/links/export",
+        {
+            schema: {
+                summary: "Export links to CSV",
+                description: "Generates a CSV file with links based on search query",
+                tags: ['Links'],
+                body: z.object({
+                    searchQuery: z.string().optional(),
+                }),
+                response: {
+                    200: z.object({
+                        reportLinks: z.string().url(),
+                    }),
+                },
+            },
+        },
+        async (request, reply) => {
+            const { searchQuery } = request.body;
+
+            const result = await exportUploads({
+                searchQuery,
+            });
+
+            const { reportLinks } = unwrapEither(result);
+
+            return reply.status(200).send({ reportLinks });
+        }
+    );
 };
