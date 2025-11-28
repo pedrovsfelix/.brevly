5f9bcd255edab784b02c6e76ecb5baa2a0121ae1
pedrovsfelix <pedrovitorsf@gmail.com>
2025-11-28 06:35:35 -0300

    :sparkles: feat: Criando lógica para deletar o link pelo ID.

--- server/src/infra/http/routes/delete-link.ts
+++ server/src/infra/http/routes/delete-link.ts
@@ -1,7 +1,40 @@
+import { deleteLink } from "@/app/functions/delete-link";
+import { isLeft } from "@/shared/either";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import z from "zod";
 
-export const deleteLinkRoute: FastifyPluginAsyncZod = async (server) => {
-	server.post("/links", () => {
-		return "hello";
-	});
+export const deleteLinkRoute: FastifyPluginAsyncZod = async server => {
+    await server.delete(
+        "/links/:id",
+        {
+            schema: {
+                summary: "Delete a link",
+                description: "Delete a link by ID",
+                tags: ['Links'],
+
+                params: z.object({
+                    id: z.string().uuid({ message: "ID inválido" }),
+                }),
+
+                response: {
+                    204: z.null().describe("Link deletado com sucesso"),
+                    400: z.object({ message: z.string() }),
+                    404: z.object({ message: z.string() }),
+                },
+            },
+        },
+        async (request, reply) => {
+            const { id } = request.params;
+
+            const result = await deleteLink({ id });
+
+            if (isLeft(result)) {
+                const error = result.left;
+        
+                return reply.status(404).send({ message: error.message });
+            }
+
+            return reply.status(204).send();
+        }
+    );
 };
