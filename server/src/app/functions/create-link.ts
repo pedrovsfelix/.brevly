68d4140c16322f63d222871c1f3fc38f2f9ca9f8
pedrovsfelix <pedrovitorsf@gmail.com>
2025-11-28 06:27:41 -0300

    :recycle: refactor: Refatorando erros da rota de criação de links.

--- server/src/app/functions/create-link.ts
+++ server/src/app/functions/create-link.ts
@@ -3,43 +3,45 @@ import { schema } from "@/infra/db/schemas";
 import { type Either, makeLeft, makeRight } from "@/shared/either";
 import z from "zod";
 import { InvalidShortUrl } from "./errors/invalid-shorturl-format";
+import { LinkAlreadyExistsError } from "./errors/link-already-exists";
 
 const createLinkInput = z.object({
-	originalUrl: z.string().url(),
-	shortUrl: z.string().regex(/^[a-z0-9-]+$/, {
-		message:
-			"Não deve ser possível criar um link com URL encurtada mal formatada",
-	}),
+    originalUrl: z.string().url(),
+    shortUrl: z.string().regex(/^[a-z0-9-]+$/, {
+        message:
+            "Não deve ser possível criar um link com URL encurtada mal formatada",
+    }),
 });
 
 type CreateLinkInput = z.input<typeof createLinkInput>;
 
 type CreateLinkOutput = {
-	id: string;
+    id: string;
 };
 
 export async function createLink(
-	input: CreateLinkInput,
-): Promise<Either<InvalidShortUrl, CreateLinkOutput>> {
-	const { originalUrl, shortUrl } = createLinkInput.parse(input);
+    input: CreateLinkInput,
+): Promise<Either<InvalidShortUrl | LinkAlreadyExistsError, CreateLinkOutput>> {
+    
+    const { originalUrl, shortUrl } = createLinkInput.parse(input);
 
-	const existingShortLink = await db.query.links.findFirst({
-		where: (links, { eq }) => eq(links.shortUrl, shortUrl),
-	});
+    const existingShortLink = await db.query.links.findFirst({
+        where: (links, { eq }) => eq(links.shortUrl, shortUrl),
+    });
 
-	if (existingShortLink) {
-		return makeLeft(new InvalidShortUrl());
-	}
+    if (existingShortLink) {
+        return makeLeft(new LinkAlreadyExistsError(shortUrl));
+    }
 
-	const link = await db
-		.insert(schema.links)
-		.values({
-			originalUrl,
-			shortUrl,
-		})
-		.returning();
+    const link = await db
+        .insert(schema.links)
+        .values({
+            originalUrl,
+            shortUrl,
+        })
+        .returning();
 
-	return makeRight({
-		id: link[0].id,
-	});
+    return makeRight({
+        id: link[0].id,
+    });
 }
