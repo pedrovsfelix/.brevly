00ba2a943090b052c280555d7336bc17a04ebd47
pedrovsfelix <pedrovitorsf@gmail.com>
2025-11-28 06:36:27 -0300

    :recycle: refactor: Alterando o schema base.

--- server/src/infra/db/schemas/links.ts
+++ server/src/infra/db/schemas/links.ts
@@ -13,6 +13,6 @@ export const links = pgTable("links", {
 		.$defaultFn(() => uuidv7()),
 	originalUrl: text("original_url").notNull(),
 	shortUrl: varchar("short_url", { length: 10 }).unique().notNull(),
-	accessClick: integer("click_count").default(0).notNull(),
+	accessClick: integer("access_click").default(0).notNull(),
 	createdAt: timestamp("created_at").defaultNow().notNull(),
 });
