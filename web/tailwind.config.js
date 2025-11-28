e730f82051e05d16622ef5f1f123b45089f492a6
pedrovsfelix <pedrovitorsf@gmail.com>
2025-11-28 06:47:27 -0300

    :heavy_plus_sign: build: Adicionando novas dependências.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,46 +1,41 @@
-import { theme as defaultTheme } from 'tailwindcss/defaultConfig';
+import { theme as defaultTheme } from "tailwindcss/defaultConfig";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-
     extend: {
-
       colors: {
-        'primary': {
-          DEFAULT: '#2C46B1',
-          'dark': '#2C4091',
+        primary: {
+          DEFAULT: "#2C46B1",
+          dark: "#2C4091",
         },
 
-        'gray': {
-          '100': '#F9F9FB',
-          '200': '#E4E6EC',
-          '300': '#CDCFD5',
-          '400': '#74798B',
-          '500': '#4D505C',
-          '600': '#1F2025',
+        gray: {
+          100: "#F9F9FB",
+          200: "#E4E6EC",
+          300: "#CDCFD5",
+          400: "#74798B",
+          500: "#4D505C",
+          600: "#1F2025",
         },
 
-        'white': '#FFFFFF',
-        'danger': '#B12C4D',
+        white: "#FFFFFF",
+        danger: "#B12C4D",
       },
 
       fontSize: {
-        'xs': '10px',
-        'sm': '12px',
-        'md': '14px',
-        'lg': '18px',
-        'xl': '24px',
+        xs: "10px",
+        sm: "12px",
+        md: "14px",
+        lg: "18px",
+        xl: "24px",
       },
 
       fontFamily: {
-        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
-      }
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
-}
+};
