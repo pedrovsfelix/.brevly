12e7b7f4983684425223e76dac73da81688cdf5f
pedrovsfelix <pedrovitorsf@gmail.com>
2025-11-28 06:53:17 -0300

    :sparkles: feat: Adicionando o toaster para as páginas.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { App } from './app.tsx'
 import './index.css'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster position="bottom-right" />
     </QueryClientProvider>
   </StrictMode>,
 )
