a84b90af82fbe03ebad60947aecf38cd883ff46b
pedrovsfelix <pedrovitorsf@gmail.com>
2025-12-24 11:13:05 -0300

    refactor: Rota para obter link por ID.

--- server/src/infra/http/routes/get-link-by-id.ts
+++ server/src/infra/http/routes/get-link-by-id.ts
@@ -1,4 +1,3 @@
-
 import { getLinkById } from "@/app/functions/get-link-by-id";
 import { isLeft, unwrapEither } from "@/shared/either";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
@@ -9,15 +8,19 @@ export const getLinkByIdRoute: FastifyPluginAsyncZod = async server => {
         "/links/:id",
         {
             schema: {
-                summary: "Get original URL",
-                description: "Get original URL and increment click count",
+                summary: "Get link by ID",
+                description: "Get a link's original URL by its ID and increment click count",
                 tags: ['Links'],
                 params: z.object({
                     id: z.string(),
                 }),
                 response: {
                     200: z.object({
+                        id: z.string(),
                         originalUrl: z.string().url(),
+                        shortUrl: z.string(),
+                        accessClick: z.number(),
+                        createdAt: z.string(),
                     }),
                     404: z.object({
                         message: z.string(),
@@ -28,17 +31,13 @@ export const getLinkByIdRoute: FastifyPluginAsyncZod = async server => {
         async (request, reply) => {
             const { id } = request.params;
 
-            const result = await getLinkById({
-                shortUrl: id,
-            });
+            const result = await getLinkById({ id });
 
             if (isLeft(result)) {
                 return reply.status(404).send({ message: result.left.message });
             }
 
-            const { originalUrl } = unwrapEither(result);
-
-            return reply.status(200).send({ originalUrl });
+            return reply.status(200).send(unwrapEither(result));
         }
     );
 };
